add42e59d868168fca8a0dc57e6c402ef5d18a6c
onsfares <onsfaress@gmail.com>
2017-01-16 10:45:31 +0100

    Update app.js

--- tp6/app.js
+++ tp6/app.js
@@ -1,6 +1,2 @@
 var meteoApp= angular.module('meteoApp', ['ngRoute', 'meteoControllers'])
-angular.module("myapp", [])
-    .view("HelloController", function($scope) {
-        $scope.helloTo = {};
-        $scope.helloTo.title = "World, AngularJS";
-    } );
+
